2ea8848a5ce8882cf668cc1cf5204b7c50a06762
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-04-30 05:38:46 +0900

    refactor(FmPlayerDetailsDto): FmPlayerDetailsDto record 로 리펙토링

--- src/main/java/com/ksy/fmrs/dto/player/FmPlayerDetailsDto.java
+++ src/main/java/com/ksy/fmrs/dto/player/FmPlayerDetailsDto.java
@@ -1,33 +1,31 @@
 package com.ksy.fmrs.dto.player;
 
 import com.ksy.fmrs.domain.player.*;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-public class FmPlayerDetailsDto {
-    private String name;
-    private Position position;
-    private int currentAbility;
-    private int potentialAbility;
-    private PersonalityAttributes personalityAttributes;
-    private TechnicalAttributes technicalAttributes;
-    private MentalAttributes mentalAttributes;
-    private PhysicalAttributes physicalAttributes;
-    private GoalKeeperAttributes goalKeeperAttributes;
-    private HiddenAttributes hiddenAttributes;
-    
-    public FmPlayerDetailsDto(FmPlayer fmPlayer) {
-        this.name = fmPlayer.getName();
-        this.position = fmPlayer.getPosition();
-        this.currentAbility = fmPlayer.getCurrentAbility();
-        this.potentialAbility = fmPlayer.getPotentialAbility();
-        this.personalityAttributes = fmPlayer.getPersonalityAttributes();
-        this.technicalAttributes = fmPlayer.getTechnicalAttributes();
-        this.mentalAttributes = fmPlayer.getMentalAttributes();
-        this.physicalAttributes = fmPlayer.getPhysicalAttributes();
-        this.goalKeeperAttributes = fmPlayer.getGoalKeeperAttributes();
-        this.hiddenAttributes = fmPlayer.getHiddenAttributes();
+public record FmPlayerDetailsDto(
+        String name,
+        Position position,
+        int currentAbility,
+        int potentialAbility,
+        PersonalityAttributes personalityAttributes,
+        TechnicalAttributes technicalAttributes,
+        MentalAttributes mentalAttributes,
+        PhysicalAttributes physicalAttributes,
+        GoalKeeperAttributes goalKeeperAttributes,
+        HiddenAttributes hiddenAttributes
+) {
+    public FmPlayerDetailsDto(FmPlayer p) {
+        this(
+                p.getName(),
+                p.getPosition(),
+                p.getCurrentAbility(),
+                p.getPotentialAbility(),
+                p.getPersonalityAttributes(),
+                p.getTechnicalAttributes(),
+                p.getMentalAttributes(),
+                p.getPhysicalAttributes(),
+                p.getGoalKeeperAttributes(),
+                p.getHiddenAttributes()
+        );
     }
 }
